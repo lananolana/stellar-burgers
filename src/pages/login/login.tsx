4ce39a12040b346c07aad3a3ce6e59c8cb8eac17
Lana Alekseeva <lanaalekseevaa@gmail.com>
2025-09-25 23:14:35 +0100

    fix post-login redirect

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,7 +2,6 @@ import { FC, SyntheticEvent } from 'react';
 import { LoginUI } from '@ui-pages';
 import { useDispatch, useSelector } from '../../services/store';
 import { loginUserApiAsync } from '../../services/slices/userSlice';
-import { useNavigate } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
 import { TLoginData } from '@api';
 
@@ -13,16 +12,11 @@ export const Login: FC = () => {
   });
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const error = useSelector((state) => state.user.error);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
-    dispatch(loginUserApiAsync(values)).then((res) => {
-      if (res.meta.requestStatus == 'fulfilled') {
-        navigate('/');
-      }
-    });
+    dispatch(loginUserApiAsync(values));
   };
 
   return (
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,14 +1,12 @@
-import { FC, SyntheticEvent, useState } from 'react';
+import { FC, SyntheticEvent } from 'react';
 import { RegisterUI } from '@ui-pages';
 import { useDispatch, useSelector } from '../../services/store';
-import { useNavigate } from 'react-router-dom';
 import { registerUserApiAsync } from '../../services/slices/userSlice';
 import { useForm } from '../../hooks/useForm';
 import { TRegisterData } from '@api';
 
 export const Register: FC = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const { values, setFieldValue } = useForm<TRegisterData>({
     email: '',
     name: '',
@@ -18,11 +16,7 @@ export const Register: FC = () => {
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
-    dispatch(registerUserApiAsync(values)).then((res) => {
-      if (res.meta.requestStatus == 'fulfilled') {
-        navigate('/');
-      }
-    });
+    dispatch(registerUserApiAsync(values));
   };
 
   return (
